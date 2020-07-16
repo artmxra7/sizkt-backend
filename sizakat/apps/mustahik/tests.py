265a05bfdfd94e2570f9f68f4ede2e7b29dca42f
addffa <adli.daffa5@gmail.com>
2020-07-16 10:54:16 +0700

    [RED] membuat test_mustahik_model

--- sizakat/apps/mustahik/tests.py
+++ sizakat/apps/mustahik/tests.py
@@ -1,3 +1,26 @@
+from datetime import date
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Mustahik
+
+
+class MustahikModelTestCase(TestCase):
+    def setUp(self):
+        Mustahik.objects.create(
+            name='mustahik',
+            no_ktp='31751234567890',
+            phone='081234567890',
+            address='Jalan raya depok',
+            province='Jawa Barat',
+            regency='Depok',
+            rt='003',
+            rw='002',
+            birthdate=date(1987, 6, 5),
+            status=Mustahik.Status.MISKIN,
+            family_size=4,
+            description='desc'
+        )
+
+    def test_mustahik_creation(self):
+        mustahik = Mustahik.objects.get(no_ktp='31751234567890')
+        self.assertTrue(isinstance(mustahik, Mustahik))
