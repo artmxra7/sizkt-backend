acdd7f6dd78a82497b4247c070ad2b0d85c2d97d
addffa <adli.daffa5@gmail.com>
2020-07-16 11:18:34 +0700

    [GREEN] implementasi model mustahik

--- sizakat/apps/mustahik/models.py
+++ sizakat/apps/mustahik/models.py
@@ -4,4 +4,25 @@ from django.db import models
 
 
 class Mustahik(models.Model):
-    pass
+
+    class Status(models.TextChoices):
+        JANDA = ('JANDA', 'Janda')
+        MISKIN = ('MISKIN', 'Miskin')
+        YATIM = ('YATIM', 'Yatim')
+
+    name = models.CharField(max_length=32)
+    no_ktp = models.CharField(max_length=32)
+    phone = models.CharField(max_length=32, blank=True)
+    address = models.TextField()
+    province = models.CharField(max_length=32)
+    regency = models.CharField(max_length=32)
+    rt = models.CharField(max_length=4)
+    rw = models.CharField(max_length=4)
+    birthdate = models.DateField()
+    status = models.CharField(
+        max_length=32,
+        choices=Status.choices,
+        default=Status.MISKIN,
+    )
+    family_size = models.PositiveSmallIntegerField()
+    description = models.TextField()
