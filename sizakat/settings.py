3c759810210a041bccc5bb5963437ffc1be504a1
addffa <adli.daffa5@gmail.com>
2020-07-17 20:16:40 +0700

    [CHORES] update cors in settings and fix path migrations in .gitignore

--- sizakat/settings.py
+++ sizakat/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'graphene_django',
+    'corsheaders',
     'sizakat.apps.mustahik'
 ]
 
@@ -48,12 +49,15 @@ GRAPHENE = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = os.environ.get('CORS_ORIGIN_WHITELIST').split()
 
 ROOT_URLCONF = 'sizakat.urls'
 
