f7ae0393772b66ed7d5e65e12246657664675caf
addffa <adli.daffa5@gmail.com>
2020-07-24 10:35:26 +0700

    [CHORES] membuat konfigurasi email backend

--- sizakat/settings.py
+++ sizakat/settings.py
@@ -136,3 +136,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+# Email config
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'adli.daffa5@gmail.com'
+EMAIL_HOST_PASSWORD = '020599adli020599'
