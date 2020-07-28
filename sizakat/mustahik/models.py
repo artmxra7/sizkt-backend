6fad5cec4b3b48702481064d77e262c3c1d5d8b5
addffa <adli.daffa5@gmail.com>
2020-07-28 17:49:34 +0700

    [GREEN] implementasi model mustahik baru

--- sizakat/mustahik/models.py
+++ sizakat/mustahik/models.py
@@ -1,3 +1,5 @@
+import os
+
 from django.db import models
 from django.utils import timezone
 
@@ -6,9 +8,13 @@ from sizakat.validators import validate_numeric_character
 
 class Mustahik(models.Model):
     class Status(models.TextChoices):
-        JANDA = ('JANDA', 'Janda')
+        FAKIR = ('FAKIR', 'Fakir')
         MISKIN = ('MISKIN', 'Miskin')
-        YATIM = ('YATIM', 'Yatim')
+        AMIL = ('AMIL', 'Amil')
+        MUALAF = ('MUALAF', 'Mualaf')
+        GHARIM = ('GHARIM', 'Gharim')
+        FISABILILLAH = ('FISABILILLAH', 'Fisabilillah')
+        MUSAFIR = ('MUSAFIR', 'Musafir')
 
     class Gender(models.TextChoices):
         LAKILAKI = ('L', 'Laki-Laki')
@@ -23,24 +29,82 @@ class Mustahik(models.Model):
         max_length=32, blank=True, null=True,
         validators=[validate_numeric_character]
     )
-    address = models.TextField()
-    province = models.CharField(max_length=32)
-    regency = models.CharField(max_length=50)
-    rt = models.CharField(
-        max_length=4, validators=[validate_numeric_character]
-    )
-    rw = models.CharField(
-        max_length=4, validators=[validate_numeric_character]
-    )
+    address = models.CharField(max_length=255)
     birthdate = models.DateField()
-    status = models.CharField(
-        max_length=32,
-        choices=Status.choices,
-        default=Status.MISKIN,
-    )
-    family_size = models.PositiveSmallIntegerField()
-    description = models.TextField(blank=True, null=True)
+    status = models.CharField(max_length=32, choices=Status.choices)
     gender = models.CharField(max_length=1, choices=Gender.choices)
+    photo = models.FileField(
+        upload_to=os.path.join('images', 'mustahik'),
+        default=os.path.join('images', 'default_photo.jpg')
+    )
+    data_source = models.ForeignKey('DataSource', on_delete=models.CASCADE)
 
     def calculate_age(self):
         return timezone.now().year - self.birthdate.year
+
+
+class DataSource(models.Model):
+    class Category(models.TextChoices):
+        WARGA = ('WARGA', 'Warga')
+        INSTITUSI = ('INSTITUSI', 'Institusi')
+        PEKERJA = ('PEKERJA', 'Pekerja')
+
+    pic_name = models.CharField(max_length=150)
+    pic_ktp = models.CharField(
+        max_length=32,
+        validators=[validate_numeric_character]
+    )
+    pic_phone = models.CharField(
+        max_length=32,
+        validators=[validate_numeric_character]
+    )
+    pic_position = models.CharField(max_length=50)
+    category = models.CharField(max_length=32, choices=Category.choices)
+
+    class Meta:
+        unique_together = ('pic_ktp', 'category',)
+
+
+class DataSourceWarga(models.Model):
+    province = models.CharField(max_length=50)
+    regency = models.CharField(max_length=50)
+    sub_district = models.CharField(max_length=50)
+    village = models.CharField(max_length=50)
+    rt = models.CharField(
+        max_length=3, validators=[validate_numeric_character]
+    )
+    rw = models.CharField(
+        max_length=3, validators=[validate_numeric_character]
+    )
+    data_source = models.OneToOneField(
+        'DataSource', on_delete=models.CASCADE,
+        limit_choices_to={'category': DataSource.Category.WARGA}
+    )
+
+
+class DataSourceInstitusi(models.Model):
+    name = models.CharField(max_length=150)
+    province = models.CharField(max_length=50)
+    regency = models.CharField(max_length=50)
+    sub_district = models.CharField(max_length=50)
+    village = models.CharField(max_length=50)
+    rt = models.CharField(
+        max_length=3, validators=[validate_numeric_character]
+    )
+    rw = models.CharField(
+        max_length=3, validators=[validate_numeric_character]
+    )
+    address = models.CharField(max_length=255)
+    data_source = models.OneToOneField(
+        'DataSource', on_delete=models.CASCADE,
+        limit_choices_to={'category': DataSource.Category.INSTITUSI}
+    )
+
+
+class DataSourcePekerja(models.Model):
+    profession = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+    data_source = models.OneToOneField(
+        'DataSource', on_delete=models.CASCADE,
+        limit_choices_to={'category': DataSource.Category.PEKERJA}
+    )
