16ea4d63864bc2cd6c39071a64574a0e40ff7036
hashlash <muh.ashlah@gmail.com>
2020-08-17 08:16:18 +0700

    [CHORES] fix wait for db script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-if [ "$DATABASE" = "postgres" ]
+if [ -n "$POSTGRES_DB" ]
 then
     echo "Waiting for postgres..."
 
-    while ! nc -z $SQL_HOST $SQL_PORT; do
+    while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
       sleep 0.1
     done
 
