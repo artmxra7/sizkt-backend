2b265ebb2df76814429bcefb4e84d1575e7a707d
hashlash <muh.ashlah@gmail.com>
2020-09-04 10:07:10 +0700

    [CHORES] add django admin for mustahik related models

--- sizakat/mustahik/admin.py
+++ sizakat/mustahik/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Mustahik,
+    DataSource,
+    DataSourceWarga,
+    DataSourceInstitusi,
+    DataSourcePekerja,
+)
+
+admin.site.register([
+    Mustahik,
+    DataSource,
+    DataSourceWarga,
+    DataSourceInstitusi,
+    DataSourcePekerja,
+])
